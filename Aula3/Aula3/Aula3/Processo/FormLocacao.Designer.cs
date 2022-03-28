2a5043ccfd532055aa16fff9c9a0c3154e793ebd
Leandro <leandrovianar9@gmail.com>
2022-03-28 19:47:07 -0300

    Aula 3

---
+++ Aula3/Aula3/Aula3/Processo/FormLocacao.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula3.Processo
+{
+    partial class FormLocacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormLocacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormLocacao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Locação de Filme";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Aula3/Aula3/Aula3/formMDI.cs
@@ -0,0 +1,48 @@
+﻿using Aula3.Cadastro;
+using Aula3.Processo;
+using System;
+using System.Windows.Forms;
+
+namespace Aula3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tsCliente_Click(object sender, EventArgs e)
+        {
+            FormCliente frmCliente = new FormCliente();
+            frmCliente.MdiParent = this;
+            frmCliente.Show();
+        }
+
+        private void tsGenero_Click(object sender, EventArgs e)
+        {
+            FormGenero frmGenero = new FormGenero();
+            frmGenero.MdiParent = this;
+            frmGenero.Show();
+        }
+
+        private void tsFilme_Click(object sender, EventArgs e)
+        {
+            FormFilme frmFilme = new FormFilme();
+            frmFilme.MdiParent = this;
+            frmFilme.Show();
+        }
+
+        private void tsLocacao_Click(object sender, EventArgs e)
+        {
+            FormLocacao frmLocacao = new FormLocacao();
+            frmLocacao.MdiParent = this;
+            frmLocacao.Show();
+        }
+    }
+}
