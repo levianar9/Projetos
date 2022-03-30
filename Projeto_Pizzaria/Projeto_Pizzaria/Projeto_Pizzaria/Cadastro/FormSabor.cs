749b703a378d6ca905c72d2f5b0206b0f324a674
Leandro <leandrovianar9@gmail.com>
2022-03-30 17:15:36 -0300

    projeto pizza

---
+++ Projeto_Pizzaria/Projeto_Pizzaria/Projeto_Pizzaria/Cadastro/FormSabor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Pizzaria.Cadastro
+{
+    public partial class FormSabor : Form
+    {
+        public FormSabor()
+        {
+            InitializeComponent();
+        }
+    }
+}
