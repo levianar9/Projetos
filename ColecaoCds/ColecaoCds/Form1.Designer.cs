d20959acdb2ec195f2fe61248b3ae89c45982638
Leandro <leandrovianar9@gmail.com>
2022-04-06 21:32:56 -0300

    aula cd

---
+++ ColecaoCds/ColecaoCds/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ColecaoCds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbAlbum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbArtista = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de CD\'s";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btSalvar);
+            this.groupBox1.Controls.Add(this.tbAno);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbArtista);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbAlbum);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbId);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(25, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(539, 261);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Id :";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(66, 23);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(84, 20);
+            this.tbId.TabIndex = 1;
+            // 
+            // tbAlbum
+            // 
+            this.tbAlbum.Location = new System.Drawing.Point(66, 61);
+            this.tbAlbum.Name = "tbAlbum";
+            this.tbAlbum.Size = new System.Drawing.Size(242, 20);
+            this.tbAlbum.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Album :";
+            // 
+            // tbArtista
+            // 
+            this.tbArtista.Location = new System.Drawing.Point(66, 102);
+            this.tbArtista.Name = "tbArtista";
+            this.tbArtista.Size = new System.Drawing.Size(242, 20);
+            this.tbArtista.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Artista :";
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(66, 142);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(84, 20);
+            this.tbAno.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ano :";
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(66, 207);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btSalvar.TabIndex = 8;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 339);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Coleção de CD\'s";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbArtista;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbAlbum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
